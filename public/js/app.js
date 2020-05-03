b1afab36965c5a67af12df4da7397646a6564e70
unknown <selenajosephine@gmail.com>
2020-05-03 21:10:35 +0530

    heroku properties updated

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ weatherForm.addEventListener("submit", (e) => {
     const location = search.value;
     messageTwo.textContent = 'Loading...';
     messageOne.textContent = '';
-    fetch('http://localhost:3000/weather?address=' + location).then(res => {
+    fetch('/weather?address=' + location).then(res => {
         res.json().then((data) => {
             if (data.error) {
                 messageTwo.textContent = data.error;
